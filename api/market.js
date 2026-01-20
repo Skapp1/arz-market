8bf1a57b679d0286e44665098db411ac0aacec1c
Skapp1 <skap1on@mail.ru>
2026-01-20 09:43:40 +0300

    Update market.js

--- api/market.js
+++ api/market.js
@@ -1,26 +1,26 @@
-// export default async function handler(req, res) {
-//  try {
-//    const server = req.query.server || "0";
-//
-//    const apiUrl = `https://api.arz.market/api/getSelectedMarketplace/${server}`;
+ export default async function handler(req, res) {
+  try {
+    const server = req.query.server || "0";
 
-//    const response = await fetch(apiUrl, {
-//      headers: {
-//        "User-Agent": "Mozilla/5.0",
-//        "Accept": "application/json"
-//      }
- //   });
+    const apiUrl = `https://api.arz.market/api/getSelectedMarketplace/${server}`;
 
-//    const text = await response.text();
+    const response = await fetch(apiUrl, {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json"
+      }
+    });
 
-  //  res.setHeader("Access-Control-Allow-Origin", "*");
-  //  res.setHeader("Content-Type", "application/json; charset=utf-8");
+    const text = await response.text();
 
-  //  res.status(200).send(text);
-//  } catch (e) {
-//    res.status(500).json({
- //     error: true,
-  //    message: e.message
-  //  });
-//  }
-// }
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+
+    res.status(200).send(text);
+  } catch (e) {
+    res.status(500).json({
+      error: true,
+      message: e.message
+    });
+  }
+ }
